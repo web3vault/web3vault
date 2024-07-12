739abb3abd2b738a629f40e2e50a06f3ca71febd
Sven Reher <dev@sreher.de>
2024-07-13 00:39:15 +0200

    read from the backend - test
    read from the backend - test
    
    read from the backend - test

--- pkg/ui/src/App.tsx
+++ pkg/ui/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    // declare the async data fetching function
+    const fetchData = async () => {
+      // get the data from the api
+      const res = await fetch("http://vault.localhost:8081/api/v0/");
+      const data = await res.json();
+      console.log("Response: ", data.message);
+
+      // set state with the result
+      setData(data);
+    };
+
+    // call the function
+    fetchData()
+      // make sure to catch any error
+      .catch(console.error);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Web3vault - EthGlobal 2024</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
