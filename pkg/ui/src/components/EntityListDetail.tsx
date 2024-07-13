ce61cc09ee5a61c0cf9a6b45bdae335e585db46f
Sven Reher <dev@sreher.de>
2024-07-13 04:31:07 +0200

    add entityListDetail component

--- pkg/ui/src/components/EntityListDetail.tsx
+++ pkg/ui/src/components/EntityListDetail.tsx
@@ -1,7 +1,19 @@
 export default function showEntityDetails({ entry }) {
   return (
-    <div className="mt-6">
-      {entry.name} - {entry.login} - {entry.password}!
+    <div className="mt-6" id={entry.id}>
+      <div>Name: {entry.name}</div>
+      <div>Login/Username: {entry.login}</div>
+      <div>Password: {entry.password}</div>
+      <div>Website: {entry.website}</div>
+      <div>Note: {entry.note}</div>
+      <div>
+        Categories:{" "}
+        <ul className="list-disc">
+          {entry.categories.map((category) => (
+            <li>category</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
